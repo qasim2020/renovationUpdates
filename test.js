72fd3b18f0048881fbcba9a14b6a68a32a7aa75b
Qasim Ali <qasimali24@gmail.com>
2019-10-22 14:01:21 +0500

    effort to automate leave plan failed.

--- test.js
+++ test.js
@@ -15,13 +15,11 @@ readXlsxFile(__dirname+'/server/leaveplan.xlsm').then((rows) => {
     return val.rank;
   });
 
-  console.table(formattedData);
-
   let longestPeriodSorted = [];
 
   let leaveSlot = 10;
 
-  for (var i = 0; i < 3; i++) {
+  for (var i = 0; i < 10; i++) {
 
     longestPeriodSorted = formattedData.sort((a,b) => b.onDutyPeriod - a.onDutyPeriod).filter(val => val.onDutyPeriod > 0);
 
@@ -30,16 +28,57 @@ readXlsxFile(__dirname+'/server/leaveplan.xlsm').then((rows) => {
       return total;
     },0)
 
+    // console.log({onLeave, leaveSlot});
+
+    console.table(longestPeriodSorted)
+
+
     while (onLeave < leaveSlot) {
       onLeave = onLeave + 1;
-      Object.assign(longestPeriodSorted[0],{
-        [`day${i}`]: 'W',
-      })
+      if (longestPeriodSorted.length < 1) continue;
+      switch (true) {
+          case /(WW)$/.test(longestPeriodSorted[0].nextLeave):
+            formattedData = updateData(formattedData, longestPeriodSorted[0], 'WWC', 10, i)
+            // console.log(`matched *WW ${longestPeriodSorted[0].nextLeave}  C`);
+            console.table(formattedData.find(val => longestPeriodSorted[0].name == val.name));
+            break;
+          case /(WC)$/.test(longestPeriodSorted[0].nextLeave):
+            formattedData = updateData(formattedData, longestPeriodSorted[0], 'WCW', 4, i)
+            // console.log(`matched *WC ${longestPeriodSorted[0].nextLeave}  W`);
+            console.table(formattedData.find(val => longestPeriodSorted[0].name == val.name));
+            break;
+          case /(CW)$/.test(longestPeriodSorted[0].nextLeave):
+            formattedData = updateData(formattedData, longestPeriodSorted[0], 'CWW', 4, i)
+            // console.log(`matched *CW ${longestPeriodSorted[0].nextLeave}  W`);
+            console.table(formattedData.find(val => longestPeriodSorted[0].name == val.name));
+            break;
+          case /(PW)$/.test(longestPeriodSorted[0].nextLeave):
+            formattedData = updateData(formattedData, longestPeriodSorted[0], 'PWW', 4, i)
+            // console.log(`matched *PW ${longestPeriodSorted[0].nextLeave}  W`);
+            console.table(formattedData.find(val => longestPeriodSorted[0].name == val.name));
+            break;
+          case /(WP)$/.test(longestPeriodSorted[0].nextLeave):
+            formattedData = updateData(formattedData, longestPeriodSorted[0], 'WPW', 4, i)
+            // console.log(`matched *WP ${longestPeriodSorted[0].nextLeave}  W`);
+            console.table(formattedData.find(val => longestPeriodSorted[0].name == val.name));
+            break;
+          default:
+            console.log(longestPeriodSorted[0].nextLeave);
+        }
+
+        longestPeriodSorted.shift();
     }
+
+    formattedData = formattedData.map(val => {
+      return Object.assign(val, {
+        onleave: (new Date()).addDays(i) - val.lastArrival < 0,
+        onDutyPeriod: Math.round(((new Date()).addDays(i) - val.lastArrival)/ 24 / 60 / 60 / 1000),
+      })
+    })
 
   };
 
-  console.table(longestPeriodSorted);
+  console.table(formattedData);
 
   // return;
     //
@@ -125,15 +164,22 @@ Date.prototype.addDays = function(days) {
     return date;
 }
 
-function updateData(data, object, string, duration) {
+function updateData(data, object, string, duration, count) {
+  // count total leaves alloted so far
+  // let count = val.reduce((total,val) => {
+  //   if (val.leave && val.leave.length > 0) return total += 1;
+  //   return total;
+  // },0)
+  //
   let updated = data.map(val => {
     if (val.name == object.name) {
+
       return Object.assign(val,{
         nextLeave: string,
-        nextLeaveDuration: duration,
         lastArrival: (new Date()).addDays(duration),
-        onDutyPeriod: Math.round((new Date() - (new Date()).addDays(duration)) / 24 / 60 / 60 / 1000),
-        onleave: new Date() - (new Date()).addDays(duration) < 0
+        onleave: true,
+        onDutyPeriod: (new Date()) - (new Date()).addDays(duration),
+        [`${((new Date()).addDays(count)).getDate()} - ${((new Date()).addDays(count)).getMonth()} - ${((new Date()).addDays(count)).getFullYear()}`]: `${duration + val.nextLeaveDuration} days ${string.substr(-1)} leave`
       })
     }
     return val;
